4fe15ad601318ef648c79727bddd9381576252d6
iamsr5675 <saurabhranjan5675@gmail.com>
2021-10-01 17:50:41 +0530

    Check a valid email

--- src/main/java/com/bl/userregistrationproblem/UserRegistration.java
+++ src/main/java/com/bl/userregistrationproblem/UserRegistration.java
@@ -39,6 +39,23 @@ public class UserRegistration {
 		else
 			System.err.println(lastName + " is an Invalid Last Name");
 	}
+	
+public static void checkValidEmail(String email){
+		
+		boolean isEmail;
+		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+		Pattern patternObject = Pattern.compile(emailRegex);
+		if (email == null) {
+            isEmail = false;
+        }
+        Matcher matcherObject = patternObject.matcher(email);
+        isEmail =  matcherObject.matches();
+        
+        if(isEmail)
+			System.out.println(email+" is a Valid Email address\n");
+		else
+			System.out.println(email+" is an Invalid Email address");
+	}
 
 	public static void main(String[] args) {
 
@@ -53,6 +70,10 @@ public class UserRegistration {
 		System.out.println("Enter Your Last Name");
 		String lastName = scanner.next();
 		checkValidLastName(lastName);
+		
+		System.out.println("Enter Your Email Address");
+		String email = scanner.next();
+		checkValidEmail(email);
 
 		scanner.close();
 
